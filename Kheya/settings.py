f69497825bac619f155869e7620051dec49901f2
Arnab <arnabchak10@yahoo.com>
2021-05-10 21:21:55 +0530

    final commit2

--- Kheya/settings.py
+++ Kheya/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kmx^iu52_u10!8+_ci#eh8nt$uc9rweytxmgzmc@!(5p%-sbvp'
+SECRET_KEY = os.environ['SECRET_KEY']
+# 'django-insecure-kmx^iu52_u10!8+_ci#eh8nt$uc9rweytxmgzmc@!(5p%-sbvp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["https://kheya.herokuapp.com/", "localhost"]
 
