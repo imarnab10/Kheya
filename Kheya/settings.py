a254b9688d82bcaaa431251478fe5513e1223665
Arnab <arnabchak10@yahoo.com>
2021-05-10 18:31:56 +0530

    static files commit

--- Kheya/settings.py
+++ Kheya/settings.py
@@ -119,6 +119,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
